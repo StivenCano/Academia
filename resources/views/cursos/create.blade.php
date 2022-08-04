83b7897db64eb8e2fb70e39bca629d331b8d4bc8
StivenCano <saguirre785@misena.edu.co>
2022-08-04 09:03:08 -0500

    Primera versión del proyecto Academia. Vamos en el método store del controlador de recursos

---
+++ resources/views/cursos/create.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('titulo', 'Crear Curso')
+
+@section('contenido')
+<form action="/cursos" method="POST" enctype="multipart/form-data">
+    @csrf
+    <br>
+        <h2>Aquí puedes crear un nuevo curso</h2>
+        <div class="form-group">
+            <label for="nombre">Nombre del curso</label>
+            <input id="nombre" class="form-control" type="text" name="nombre">
+        </div>
+        <div class="form-group">
+            <label for="descripcion">Descripción</label>
+            <input id="descripcion" class="form-control" type="text" name="descripcion">
+        </div>
+        <div class="form-group">
+            <label for="duracion">Duración</label>
+            <input id="duracion" class="form-control" type="text" name="duracion">
+        </div>
+        <div class="form-group">
+            <label for="imagen">Cargue la imagen del curso</label>
+            <br>
+            <input id="imagen"  type="file" name="imagen">
+
+        </div>
+        <button class="btn btn-info" type="submit">Crear</button>
+    </div>
+</form>
+@endsection
---
+++ resources/views/cursos/nosotros.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('titulo', 'Sobre Nosotros')
+
+@section('contenido')
+
+    <h2>Bienvenido a la Academia</h2>
+    <p>En nuestros días, cuando hablamos de academia o alguien hace referencia a una,
+        inmediatamente, sabemos que se está refiriendo a un establecimiento docente o a aquellas sociedades que pueden ser científicas,
+        literarias, artísticas y que tienen por objetivo el fomento, desarrollo y el aprendizaje de alguna
+        actividad cultural o científica como ser la literatura, la danza, la lengua o la promoción de una ciencia específica o de alguna especialidad relacionada a estas y que mayormente
+        han sido establecidas gracias a la voluntad económica de un estado, gobierno, empresa u organización.</p>
+
+@endsection
